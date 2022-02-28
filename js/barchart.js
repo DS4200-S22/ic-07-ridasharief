c9f173da677afaa8b2dc88444d85574d309d991e
ridasharief <sharief.r@northeastern.edu>
2022-02-28 13:04:26 -0500

    barchart 2

--- js/barchart.js
+++ js/barchart.js
@@ -123,9 +123,78 @@ svg1.selectAll(".bar") // select everything from the class bar for svg 1
      .on("mouseleave", mouseleave1);
 
 
+// create new svg object for csv based barchart
+const svg2 = d3
+  .select("#csv-bar")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
 
+// To do this, we'll need a python simple server 
+// (python3 -m http.server) running from the directory that
+// our code is in. 
+
+d3.csv("barchart.csv").then((data2) => { 
+
+  // let's check our data
+  console.log(data);
 
 
+// TODO: Find the maximum y value provided within 'data1' by going through scores column
+let maxY2 = d3.max(data2, function(d) { return d.score; });
+
+// TODO: For y value, lists what pixel value to plot, linearly scales data 
+let yScale2 = d3.scaleLinear()
+            .domain([0,maxY2])
+            .range([height-margin.bottom,margin.top]); 
+
+// TODO: For x values, lists what pixel value to add to plot
+let xScale2 = d3.scaleBand() // uses scaleBand rather than linearly scaling data
+            .domain(d3.range(data2.length))
+            .range([margin.left, width - margin.right])
+            .padding(0.1); 
+
+ // Tooltip Set-up  
 
 
+// TODO:  
+const tooltip2 = d3.select("#csv-bar") 
+                .append("div") 
+                .attr('id', "tooltip2") 
+                .style("opacity", 0) 
+                .attr("class", "tooltip"); 
+
+// TODO: when the mouse hovers over data, display data name, score, and changes opacity to 1 from 0 
+const mouseover2 = function(event, d) {
+  tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
+          .style("opacity", 1);  
+}
+
+// TODO: when the tool tip moves, the labels move with the mouse
+const mousemove2 = function(event, d) {
+  tooltip2.style("left", (event.x)+"px") 
+          .style("top", (event.y + yTooltipOffset) +"px"); 
+}
+
+// TODO: when the mouse leaves the bar, make the tool tip transparent again
+const mouseleave2 = function(event, d) { 
+  tooltip1.style("opacity", 0); 
+
+
+// add bars with styling 
+svg5.selectAll("bar") 
+      .data(data2) // this is passed into the anonymous function
+      .enter()  
+      .append("rect") // appends a rectangle to svg 1 for each row in data1
+        .attr("class", "bar") // assign the bar class 
+        .attr("x", (d,i) => xScale2(i)) // setting x position for the rectangles based on the data and what row we are on
+        //, return x scale of the row we are on
+        .attr("y", (d) => yScale2(d.score)) // setting y position based on yscale of the score
+        .attr("height", (d) => (height - margin.bottom) - yScale2(d.score)) // set height for the bars
+        .attr("width", xScale2.bandwidth())
+        .on("mouseover", mouseover2)
+        .on("mousemove", mousemove2)
+        .on("mouseleave", mouseleave);
+};
